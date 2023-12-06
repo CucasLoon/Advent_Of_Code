6dfe3f85597d34e58c3b7ec6a8fff7c37d4dbe8a
Lucas Coon <lucascoon@yahoo.com>
2023-12-06 17:22:30 -0500

    Updated Day 5
    Working on separating out the mapping

--- Advent of Code/Day_5.py
+++ Advent of Code/Day_5.py
@@ -29,17 +29,75 @@ def find_seeds(seed_row):
     num.remove("")
     return(num)
 
-def seed_to_soil():
-    output=0
+def seed_to_soil(seed):
+    output=seed
     print("output: ", output)
 
-def seed_to_soil():
-    pass
-
+def find_mappings(input):
+    counter = -1
+    print(input)
+    for x in range(len(input)):
+        if(str(input[x][0]).isalpha):
+            counter+=1
+            break
+        elif(input[x] == "\n"):
+            break
+        else:
+            temp = str(input[x]).replace("",",")
+            nums = temp
+            print(nums)
+        if counter ==0:
+            try:
+                for y in range(len(nums)):
+                    seed_to_soil_map.append(nums[y])
+            except:
+                pass
+        elif counter == 1:
+            try:
+                for y in range(len(nums)):
+                    soil_to_fert_map.append(nums[y])
+            except:
+                pass
+        elif counter == 2:
+            try:
+                for y in range(len(nums)):
+                    fert_to_water_map.append(nums[y])
+            except:
+                pass
+        elif counter == 3:
+            try:
+                for y in range(len(nums)):
+                    water_to_light_map.append(nums[y])
+            except:
+                pass
+        elif counter == 4:
+            try:
+                for y in range(len(nums)):
+                    light_to_temp_map.append(nums[y])
+            except:
+                pass
+        elif counter == 5:
+            try:
+                for y in range(len(nums)):
+                    temp_to_humid_map.append(nums[y])
+            except:
+                pass
+        elif counter == 6:
+            try:
+                for y in range(len(nums)):
+                    humid_to_location_map.append(nums[y])
+            except:
+                pass
+        else:
+            print("whoopsie")
+            return
 
 seeds = find_seeds(input_buffer[0])
 print(seeds)
-
+find_mappings(input_buffer[0:])
+for x in range(len(seeds)):
+    seed_to_soil(seeds[x])
+print(humid_to_location_map)
 ##Main will find the starting list of seeds, store that to the seeds location
 ##After seeds are stored, will walk through from seed down to location in the end
 ### will find look for the location of source, then match the destination
