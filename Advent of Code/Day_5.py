02423788ba68b3cffed994a5ac9d772771023b48
Lucas Coon <lucascoon@yahoo.com>
2023-12-06 13:20:46 -0500

    Starting Info for Day 5
    Added Start for Day 5

---
+++ Advent of Code/Day_5.py
@@ -0,0 +1,38 @@
+##Day 5 Advent of Code:
+
+input_file = "example.txt"
+
+input_buffer = []
+
+with open(input_file) as f:
+    for lines in f:
+        input_buffer.append(lines)
+
+print(input_buffer)
+
+seeds                   = []
+##Mapping
+seed_to_soil_map        = []
+soil_to_fert_map        = []
+fert_to_water_map       = []
+water_to_light_map      = []
+light_to_temp_map       = []
+temp_to_humid_map       = []
+humid_to_location_map   = []
+
+def find_seeds():
+    print("look for seeds")
+
+def seed_to_soil():
+    output=0
+    print("output: ", output)
+
+def seed_to_soil():
+    pass
+
+##Main will find the starting list of seeds, store that to the seeds location
+##After seeds are stored, will walk through from seed down to location in the end
+### will find look for the location of source, then match the destination
+
+
+
