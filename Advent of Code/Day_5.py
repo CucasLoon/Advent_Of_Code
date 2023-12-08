0b1aebbc062b3c717b95a8bc1d7ceac9e1633167
Lucas Coon <lucascoon@yahoo.com>
2023-12-08 17:06:32 -0500

    Part 1 Done
    Need to update my control over Method 2.
    Write the "new Seeds" to a text output, and read each one, convert each one, write the mapping to a new file, then walk through the file to find a minimum value

--- Advent of Code/Day_5.py
+++ Advent of Code/Day_5.py
@@ -1,6 +1,6 @@
 ##Day 5 Advent of Code:
 
-input_file = "example.txt"
+input_file = "real.txt"
 
 input_buffer = []
 
@@ -8,7 +8,7 @@ with open(input_file) as f:
     for lines in f:
         input_buffer.append(lines)
 
-print(input_buffer)
+#print(input_buffer)
 
 
 ##Mapping
@@ -21,7 +21,7 @@ temp_to_humid_map       = []
 humid_to_location_map   = []
 
 def find_seeds(seed_row):
-    print("look for seeds")
+    #print("look for seeds")
     temp = str(seed_row)
     temp=temp.replace("\n","")
     split = temp.split(":")
@@ -31,13 +31,13 @@ def find_seeds(seed_row):
 
 def seed_to_soil(seed):
     output=seed
-    print("output: ", output)
+    #print("output: ", output)
 
 def find_mappings(input):
     counter = -2
-    print(len(input))
+    ##print(len(input))
     for x in range(len(input)):
-        #print(x)
+        ##print(x)
         if(str(input[x][0]).isalpha()):
             counter+=1
         elif(input[x] == "\n"):
@@ -47,9 +47,9 @@ def find_mappings(input):
             temp = temp.replace("\n","")
             nums = temp
             nums=nums.split(",")
-            print(input[x])
-            print(temp)
-            print(nums)
+            ##print(input[x])
+            ##print(temp)
+            ##print(nums)
         if counter ==0:
             try:
                 for y in range(len(nums)):
@@ -96,7 +96,7 @@ def find_mappings(input):
             pass
 
         else:
-            print("whoopsie")
+            #print("whoopsie")
             return
 
 def sort_mappings(test):
@@ -114,20 +114,52 @@ def sort_mappings(test):
         else:
             dist.append(test[x])
             count=0
-    print(source,dest,dist)
+    ##print(source,dest,dist)
     lister = [source,dest,dist]
     return list(lister)
 
 
+## NEED TO WRITE TO A SEPERATE TEXT FILE, AND THEN READ EACH ONE INDIVUALLY
+def new_seed(seed1,length):
+    #print(seed1,length)
+    new_list = []
+    for x in range(int(length)):
+        new_list.append(int(seed1)+x)
+    return(new_list)
+
+
 seeds = find_seeds(input_buffer[0])
-print(seeds)
+seed_store = 0
+seed_list = []
+while seed_store<len(seeds):
+    temp = new_seed(seeds[seed_store],seeds[seed_store+1])
+    seed_store+=2
+    for x in range(len(temp)):
+        seed_list.append(temp[x])
+
+seeds.clear()
+seeds=seed_list
 find_mappings(input_buffer[0:])
 for x in range(len(seeds)):
     seed_to_soil(seeds[x])
 
+def convert(seed,map):
+    
+    for x in range(len(map[0])):
+        #print(map[1][x],":::",(int(map[1][x])+int(map[2][x])))
+        if int(map[1][x])<=int(seed)<(int(map[1][x])+int(map[2][x])):
+            # #print((int(seed_to_soil_map[1][x])))
+            # #print((int(seed_to_soil_map[1][x])+int(seed_to_soil_map[0][x])))
+            #print("converting:",seed,"to:",seed+(int(map[0][x])-int(map[1][x])))
+            return(seed+(int(map[0][x])-int(map[1][x])))
+        else:
+            pass
+            ##print("No Change to:",seed)
+    return seed
+
 ## Updating Maps
 
-print(seed_to_soil_map)
+#print(seed_to_soil_map)
 newer=sort_mappings(seed_to_soil_map)
 seed_to_soil_map.clear()
 seed_to_soil_map = newer
@@ -157,8 +189,20 @@ humid_to_location_map.clear()
 humid_to_location_map = temp
 
 
+final_maps = [seed_to_soil_map,soil_to_fert_map,fert_to_water_map,water_to_light_map,light_to_temp_map,temp_to_humid_map,humid_to_location_map]
+#print(final_maps[1])
+final_output = []
+for x in range(len(seeds)):
+    initial_input = int(seeds[x])
+    for y in range(len(final_maps)):
+        if y == 0:
+            input=convert(int(initial_input), final_maps[y])
+        else:
+            
+            input=convert(input, final_maps[y])
+    final_output.append(input)
 
-
+print(min(final_output))
 ##Main will find the starting list of seeds, store that to the seeds location
 ##After seeds are stored, will walk through from seed down to location in the end
 ### will find look for the location of source, then match the destination
